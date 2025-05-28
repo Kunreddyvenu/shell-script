a1f3ecd2379bf3b47c9d991ee78b1336d6ff4109
Venu Kunreddy (CW) <Venu.Kunreddy@jda.com>
2025-05-28 23:28:34 +0530

    hello venu

---
+++ some-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo " Suresh:: Hello Ramesh, how are you"
+
+echo "Ramesh:: Hello Suresh I am good what about you"
+
+echo "Suresh:: I am also good and I am thinking to learn devops"
+
+echo " Ramesh:: Great suresh"
