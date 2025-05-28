1be45ac804f1e2ac8df3d741026192847b299236
Venu Kunreddy (CW) <Venu.Kunreddy@jda.com>
2025-05-28 23:40:21 +0530

    hello venu

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+Person1=Suresh # no space between equal and value
+Person2=Ramesh
+
+echo "$Person1:: Hello $Person2, how are you"
+
+echo "$Person2:: Hello $Person1 I am good what about you"
+
+echo "$Person1:: I am also good and I am thinking to learn devops"
+
+echo "$Person2:: Great $Person1"
