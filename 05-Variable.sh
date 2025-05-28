b951505e16e3a5ab83dad8753ab997af34826cfe
Venu Kunreddy (CW) <Venu.Kunreddy@jda.com>
2025-05-28 23:59:46 +0530

    hello venu

---
+++ 05-Variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter username::"
+
+read Username #here user name is variable
+
+echo "Please enter the password::"
+
+read "Password"
+
+echo "username is: $username, Password is $Password"
