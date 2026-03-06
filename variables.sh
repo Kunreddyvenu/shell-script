8b4c9a1681fe9d39a86c617811dbb97d427114bf
Venu Kunreddy (CW) <Venu.Kunreddy@jda.com>
2026-03-06 21:13:16 +0530

    some messege

--- variables.sh
+++ variables.sh
@@ -1,1 +1,6 @@
-this is again sample script
+#!/bin/bash
+
+echo "Hi suresh how are you"
+echo "Hi ramesh i am fine how are you?"
+echo "suresh:: how is your work going"
+echo "ramesh::my work going good I am thinking to upgrde to devops"
