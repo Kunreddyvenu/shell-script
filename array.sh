3a026f36bd340c83c8271aa29b40165533a7add2
venu <kunreddyvenu88@gmail.com>
2026-03-06 22:22:41 +0530

    some messege

--- array.sh
+++ array.sh
@@ -5,7 +5,7 @@ movies=("RRR" "eega" "djtillu" "bahubali")
 # size of above value is 4
 # index is 0.1.2,3
 
-echo "first movie is $movies{[0]}"
-echo "first movie is $movies{[1]}"
-echo "first movie is $movies{[@]}"
+echo "first movie is: ${movies{[0]}"
+echo "first movie is:  ${movies{[1]}"
+echo "first movie is:  ${movies{[@]}"
 # here @ means all values 
