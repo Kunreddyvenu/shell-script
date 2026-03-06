2a00f00eecf983672f6e63e8706329202ec44660
Venu Kunreddy (CW) <Venu.Kunreddy@jda.com>
2026-03-06 21:17:56 +0530

    some messege

--- variables.sh
+++ variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Hi suresh how are you"
-echo "Hi ramesh i am fine how are you?"
-echo "suresh:: how is your work going"
-echo "ramesh::my work going good I am thinking to upgrde to devops"
+person1=Ramesh
+person2=suresh
+
+echo "Hi $person1 how are you"
+echo "Hi $person2 i am fine how are you?"
+echo "$person1:: how is your work going"
+echo "$person2::my work going good I am thinking to upgrde to devops"
