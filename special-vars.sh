74b4a22b20a399d77af473e7f946b2c7ab8c9091
venu <kunreddyvenu88@gmail.com>
2026-03-06 23:03:48 +0530

    some messege

---
+++ special-vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "all variables : $@"
+echo "Number of variables passed: $#"
+echo "script name: $0"
+echo "current working directory: $PWD"
+echo "Home directly of current user: $HOME"
+echo "wich user is running this script: $USER"
+echo "Hostname: $HOSTNAME"
+echo "process ID of the curret sell script: $$"
+echo "process id of last background command: $!"
+# thease all are special variables 
+# asks in interviews
