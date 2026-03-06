5f88cb4d6faf032de1149a65286d3fdad8e7f4b9
venu <kunreddyvenu88@gmail.com>
2026-03-06 23:13:00 +0530

    some messege

--- special-vars.sh
+++ special-vars.sh
@@ -7,6 +7,7 @@ echo "Home directly of current user: $HOME"
 echo "wich user is running this script: $USER"
 echo "Hostname: $HOSTNAME"
 echo "process ID of the curret sell script: $$"
+sleep 30 &
 echo "process id of last background command: $!"
 # thease all are special variables 
 # asks in interviews
