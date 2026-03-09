387d5c744c3fe648d8de6f9cb3e18651a4fecb39
venu <kunreddyvenu88@gmail.com>
2026-03-09 21:35:25 +0530

    some messege

--- installation.sh
+++ installation.sh
@@ -3,25 +3,20 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
-
 then
-
-  echo " please run this script with root access"
-  exit 1 # manually exit if error comes.
-  else
-  echo "you are super user"
-
-  fi
+   echo " please run this script with root access"
+   exit 1 # manually exit if error comes.
+else
+    echo "you are super user"
+fi
 
 dnf install mysqll -y
 
 if [ $? -ne 0 ]
-
 then 
    echo "installation of my sql failure"
    exit 1
-
-   fi
+ fi
 
    dnf install git -y
 
