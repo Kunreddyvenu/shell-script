e45590967608c648ed80822e2cc9390837c476f1
venu <kunreddyvenu88@gmail.com>
2026-03-10 07:06:17 +0530

    some messege

---
+++ installation2.sh
@@ -0,0 +1,26 @@
+USERID=$( id -u )
+
+if [ $USERID -ne 0]
+then
+   echo "pleae run the script with super user"
+   exit 1 # manually we off the script process if we initiate exit 1
+else
+   echo "you are a super user"
+if
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+   echo "installation of mysql failure"
+   exit 1
+else
+   echo "instllation of mysql sucess"
+fi
+dnf install Git -y
+if [ $? -ne 0 ]
+then 
+    echo "instllation of git failure"
+else
+   echo "installation of git success"
+fi
+echo "is script still proceeding?"           
