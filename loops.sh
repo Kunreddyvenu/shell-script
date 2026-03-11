5370e37b2068584157a964af2d5eee91606ab086
venu <kunreddyvenu88@gmail.com>
2026-03-11 22:23:18 +0530

    log file

--- loops.sh
+++ loops.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-for i in {A..M} # here i means we can consider its varibale it will loop from 1 to 20
+for i in {1..20} # here i means we can consider its varibale it will loop from 1 to 20
 do
-   echo $2
+   echo $1
 done   
