1466afe47cd121ef3b2fbb956b53bf08586f1ba0
venu <kunreddyvenu88@gmail.com>
2026-03-11 22:04:28 +0530

    log file

--- functions.sh
+++ functions.sh
@@ -3,13 +3,17 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+echo "script started executing at : $TIMESTAMP"
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
-      echo "$2 .....failure"
+      echo -e "$2 .....$R failure $N"
       exit 1
     else
-    echo "$2 .....sucess"
+    echo -e "$2 .....$G sucess $N"
     fi 
 }
 
@@ -26,5 +30,5 @@ VALIDATE $? "installaing my sql"
 dnf install git -y &>>$LOGFILE
 VALIDATE $? "installing git"
 
-dnf install nginx -y &>>$LOGFILE
+dnf install nginxx -y &>>$LOGFILE
 VALIDATE $? "installing nginx"
