4ede0eaf3e73e89e69326198d6f123916385419a
venu <kunreddyvenu88@gmail.com>
2026-03-13 23:20:53 +0530

    some messege

--- myown.sh
+++ myown.sh
@@ -5,12 +5,12 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
-Y="\e\33m"
+Y="\e[33m"
 N="\e[0m"
 
 
 VALIDATE (){
-    if [$1 -ne 0 ]
+    if [ $1 -ne 0 ]
     then
        echo -e "$2 ..$R FAILURE $N"
        exit 1
@@ -20,7 +20,7 @@ VALIDATE (){
       fi  
     }
 
-      if [ $USERID -ne 0]
+      if [ $USERID -ne 0 ]
         then 
             echo "please run this script with root access."
             exit 1
@@ -31,19 +31,19 @@ VALIDATE (){
       dnf install nginx -y &>>$LOGFILE
       VALIDATE $? "installing nginx"
       
-      systemctl enable nginx -y &>>$LOGFILE
+      systemctl enable nginx &>>$LOGFILE
       VALIDATE $? "enabling nginx"
 
-      systemctl start nginx -y &>>$LOGFILE
+      systemctl start nginx &>>$LOGFILE
       VALIDATE $? "Startign nginx"
 
-      rm-rf /usr/share/nginx.html/* &>>$LOGFILE
+      rm -rf /usr/share/nginx/html/* &>>$LOGFILE
       VALIDATE $? "Remvoing default content"
 
-      curl-o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+      curl -L -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
       VALIDATE $? "downloading front end code"
 
-      cd /user/share/nginx/html &>>$LOGFILE
+      cd /usr/share/nginx/html &>>$LOGFILE
       unzip /tmp/frontend.zip &>>$LOGFILE
       VALIDATE $? "extracting front end code"
 
